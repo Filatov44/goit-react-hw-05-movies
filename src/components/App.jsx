61440b5911eb2df0866793107957094da9ad0d34
Filatov44 <filatov300@gmail.com>
2022-10-30 13:26:34 +0300

    add lazy load, loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,45 @@
 import { Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import { Loader } from './Loader/Loader';
+
+// Переделываем импорты для постепенной загрузки страниц
 import Container from './Container/Container';
 import AppBar from './AppBar/AppBar';
-import Home from 'pages/Home/Home';
-import Movies from 'pages/Movies/Movies';
-import MovieDetails from 'pages/MovieDetails/MovieDetails';
-import Cast from 'pages/Cast/Cast';
-import Reviews from 'pages/Reviews/Reviews';
-import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
+
+const Home = lazy(() => import('pages/Home/Home'));
+const Movies = lazy(() => import('pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('pages/Cast/Cast'));
+const Reviews = lazy(() => import('pages/Reviews/Reviews'));
+const NotFoundPage = lazy(() => import('pages/NotFoundPage/NotFoundPage'));
+
+
+
+// import Container from './Container/Container';
+// import AppBar from './AppBar/AppBar';
+// import Home from 'pages/Home/Home';
+// import Movies from 'pages/Movies/Movies';
+// import MovieDetails from 'pages/MovieDetails/MovieDetails';
+// import Cast from 'pages/Cast/Cast';
+// import Reviews from 'pages/Reviews/Reviews';
+// import NotFoundPage from 'pages/NotFoundPage/NotFoundPage';
 
 export const App = () => {
   return (
     <>
       <Container>
         <AppBar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} >
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={ <Reviews/>} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId" element={<MovieDetails />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </Suspense>
       </Container>
     </>
   );
